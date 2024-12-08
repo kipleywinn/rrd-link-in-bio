52e202eae12a0c1509eab86356720447dc77b965
Glitch (glitch-in-bio) <none>
2024-12-08 19:14:52 +0000

    🍚🍕 Checkpoint ./templates/footer.js:39412583/1442 ./scripts/render.js:39412583/48 ./settings.json:39412583/2410 ./templates/social-svgs.js:39412583/1536

--- templates/footer.js
+++ templates/footer.js
@@ -1,21 +1,33 @@
 import { html } from "lighterhtml";
 import settings from "../settings.json";
 
-export const renderFooter = html.node`<footer class="footer">
-  <div class="tagline">
-    <b>Glitch in Bio</b><br />
-    Your links, your way.
-  </div>
-  <a
-    class="btn--remix"
-    target="_top"
-    href="https://glitch.com/edit/#!/remix/glitch-in-bio"
-  >
-    <img
-      src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140"
-      alt=""
-    />
-    Get your own free page
-  </a>
+export const renderFooter = html.node`<footer class="afooter">
+  <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            <div>
+              <input type="hidden" name="cmd" value="_donations" />
+              <input type="hidden" name="bn" value="TipsandTricks_SP" />
+              <input
+                type="hidden"
+                name="business"
+                value="rockstarrollerderby@gmail.com"
+              />
+              <input type="hidden" name="rm" value="0" />
+              <input type="hidden" name="currency_code" value="USD" />
+              <input
+                type="image"
+                style="cursor: pointer"
+                src="https://cdn.glitch.me/089607c7-7361-4d68-88c6-455c670decf8%2Fpp-donate.png?v=1637254704665"
+                width="75%"
+                name="submit"
+                alt="PayPal - The safer, easier way to pay online."
+              />
+              <img
+                alt=""
+                src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
+                width="1"
+                height="1"
+              />
+            </div>
+          </form>
 </footer>
 `;
